d1b414b3cf1631a0219d697015653c3005f629a1
hardik-agrl <hardik2001agarwal@gmail.com>
2025-06-02 17:30:23 +0530

    added window function query also

--- User_conversion.sql
+++ User_conversion.sql
@@ -7,9 +7,21 @@ CREATE TABLE ecommerce.customers (
 );
 
 
-SELECT COUNT(User_Id) AS UserCount
+SELECT COUNT(DISTINCT User_Id) AS UserCount
 FROM ecommerce.customers
 WHERE Offer_notification_click_time IS NOT NULL
   AND Purchase_completion_time IS NOT NULL
   AND Purchase_completion_time >= Offer_notification_click_time
   AND Purchase_completion_time BETWEEN Session_start AND Session_end;
+
+
+-- Using Window Function
+SELECT DISTINCT
+  SUM(CASE
+        WHEN Offer_notification_click_time IS NOT NULL
+         AND Purchase_completion_time IS NOT NULL
+         AND Purchase_completion_time >= Offer_notification_click_time
+         AND Purchase_completion_time BETWEEN Session_start AND Session_end
+        THEN 1 ELSE 0
+      END) OVER () AS UserCount
+FROM ecommerce.customers;
